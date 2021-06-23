6af4fca4aa919773b570291902ee323d1c18ec0c
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-23 18:05:54 -0300

    add google signin

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -17,48 +18,68 @@ class _LoginPageState extends State<LoginPage> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-        backgroundColor: AppColors.background,
-        body: Container(
-            width: size.width,
-            height: size.height,
-            child: Stack(children: [
-              Container(
-                width: size.width,
-                height: size.height * 0.36,
-                color: AppColors.primary,
-              ),
-              Positioned(
-                  top: 40,
-                  left: 0,
-                  right: 0,
-                  child: Image.asset(
-                    AppImages.person,
-                    width: 208,
-                    height: 350,
-                  )),
-              Positioned(
-                bottom: size.height * 0.05,
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.36,
+              color: AppColors.primary,
+            ),
+            Positioned(
+                top: 40,
                 left: 0,
                 right: 0,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(AppImages.logomini),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 30, left: 70, right: 70),
-                        child: Text("Organize seus boletos em um só lugar",
-                            textAlign: TextAlign.center,
-                            style: TextStyles.titleHome),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(left: 40, right: 40, top: 40),
-                        child: SocialLoginButton(onTap: () => print("clicou")),
-                      )
-                    ]),
-              )
-            ])));
+                child: Image.asset(
+                  AppImages.person,
+                  width: 208,
+                  height: 350,
+                )),
+            Positioned(
+              bottom: size.height * 0.05,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text("Organize seus boletos em um só lugar",
+                        textAlign: TextAlign.center,
+                        style: TextStyles.titleHome),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () async {
+                        var _google_signin = GoogleSignIn(
+                          scopes: [
+                            'email',
+                          ],
+                        );
+
+                        try {
+                          final response = await _google_signin.signIn();
+                          print(response);
+                        } catch (error) {
+                          print(error);
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
