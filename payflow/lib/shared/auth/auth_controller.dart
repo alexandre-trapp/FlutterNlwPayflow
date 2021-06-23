71511c92ef0aa2dcf3aa9b3a86badb83d1b67559
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-23 19:19:06 -0300

    add user_model / ajuste auth_controller

--- payflow/lib/shared/auth/auth_controller.dart
+++ payflow/lib/shared/auth/auth_controller.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   var _isAuthenticated = false;
 
-  var _user;
-  get user => _user;
+  UserModel? _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
       _user = user;
       _isAuthenticated = true;
@@ -18,18 +19,18 @@ class AuthController {
     }
   }
 
-  Future<void> saveUser(var user) async {
+  Future<void> saveUser(UserModel user) async {
     final instance = await SharedPreferences.getInstance();
-    await instance.setString("user", user);
+    await instance.setString("user", user.toJson());
 
     return;
   }
 
   Future<void> currentUser(BuildContext context) async {
     final instance = await SharedPreferences.getInstance();
-    final user = instance.get("user");
+    final userJson = instance.get("user") as String;
 
-    setUser(context, user);
+    setUser(context, UserModel.fromJson(userJson));
     return;
   }
 }
