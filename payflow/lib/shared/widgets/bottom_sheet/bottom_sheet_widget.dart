a4d6ecdf06b280f042b3382504ab51d9de36bac9
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-24 20:46:22 -0300

    styles textos

--- payflow/lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
+++ payflow/lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BottomSheetWidget extends StatelessWidget {
@@ -41,7 +42,13 @@ class BottomSheetWidget extends StatelessWidget {
                   child: Text.rich(
                     TextSpan(
                       text: title,
-                      children: [TextSpan(text: "\n$subtitle")],
+                      style: TextStyles.buttonBoldHeading,
+                      children: [
+                        TextSpan(
+                          text: "\n$subtitle",
+                          style: TextStyles.buttonHeading,
+                        )
+                      ],
                     ),
                     textAlign: TextAlign.center,
                   ),
