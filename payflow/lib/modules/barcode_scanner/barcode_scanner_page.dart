68afa81cc277f05844004ca48a69a9a93a08091d
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-24 13:59:13 -0300

    add bottomNavigationBar

--- payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/label_buttons/label_button.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -13,23 +14,24 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Escaneie o código de barras do boleto",
-          style: TextStyles.buttonBackground,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "Escaneie o código de barras do boleto",
+            style: TextStyles.buttonBackground,
+          ),
+          leading: BackButton(
+            color: AppColors.background,
+          ),
         ),
-        leading: BackButton(
-          color: AppColors.background,
+        body: Column(
+          children: [
+            Expanded(child: Container(color: Colors.black)),
+            Expanded(child: Container(color: Colors.transparent)),
+            Expanded(child: Container(color: Colors.black)),
+          ],
         ),
-      ),
-      body: Column(
-        children: [
-          Expanded(child: Container(color: Colors.black)),
-          Expanded(child: Container(color: Colors.transparent)),
-          Expanded(child: Container(color: Colors.black)),
-        ],
-      ),
-    );
+        bottomNavigationBar:
+            LabelButton(label: "Inserir código do boleto", onPressed: () {}));
   }
 }
