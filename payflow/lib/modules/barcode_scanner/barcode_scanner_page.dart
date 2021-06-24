453a42040342cbf9fad90e4e048affd17e9577cd
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-24 20:22:12 -0300

    criado SetLabelButtons

--- payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -3,6 +3,7 @@ import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_buttons/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   BarcodeScannerPage({Key? key}) : super(key: key);
@@ -32,25 +33,11 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
           Expanded(child: Container(color: Colors.black)),
         ],
       ),
-      bottomNavigationBar: Container(
-        height: 56,
-        child: Row(
-          children: [
-            Expanded(
-              child: LabelButton(
-                label: "Inserir código do boleto",
-                onPressed: () {},
-              ),
-            ),
-            DividerVerticalWidget(),
-            Expanded(
-              child: LabelButton(
-                label: "Adicionar da galeria",
-                onPressed: () {},
-              ),
-            ),
-          ],
-        ),
+      bottomNavigationBar: SetLabelButtons(
+        primaryLabel: "Código do boleto",
+        primaryOnPressed: () {},
+        secondaryLabel: "Adicionar da galeria",
+        secondaryOnPressed: () {},
       ),
     );
   }
