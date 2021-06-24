50ff442c3a405587204cc50beed7fca6300e815c
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-24 20:33:05 -0300

    criado BottomSheetWidget

--- payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
-import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
-import 'package:payflow/shared/widgets/label_buttons/label_button.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -15,6 +14,8 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
+    return BottomSheetWidget();
+
     return SafeArea(
       top: true,
       bottom: true,
