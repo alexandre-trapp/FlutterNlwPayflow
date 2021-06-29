2856f888511f240efbd2866906b6b2dd588481c6
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-06-28 22:14:49 -0300

    criação barcodeScanner

--- payflow/lib/modules/barcode_scanner/barcode_scanner_controller.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_controller.dart
@@ -1,9 +1,13 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 import 'barcode_scanner_status.dart';
 
 class BarcodeScannerController {
   BarcodeScannerStatus status = BarcodeScannerStatus();
+  final barcodeScanner = GoogleMlKit.vision.barcodeScanner();
 
   void getAvailableCameras() async {
     try {
@@ -22,5 +26,82 @@ class BarcodeScannerController {
     } catch (e) {
       status = BarcodeScannerStatus.error(e.toString());
     }
+  }
+
+  Future<void> scannerBarCode(InputImage inputImage) async {
+    try {
+      if (status.cameraController != null &&
+          status.cameraController!.value.isStreamingImages) {
+        status.cameraController!.stopImageStream();
+      }
+
+      final barcodes = await barcodeScanner.processImage(inputImage);
+      var barcode;
+
+      for (Barcode item in barcodes) {
+        barcode = item.value.displayValue;
+      }
+
+      if (barcode != null && status.barcode.isEmpty) {
+        status = BarcodeScannerStatus.barcode(barcode);
+        status.cameraController!.dispose();
+      } else {
+        getAvailableCameras();
+      }
+
+      return;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void listenCamera() {
+    if (!status.cameraController!.value.isStreamingImages)
+      status.cameraController!.startImageStream(
+        (cameraImage) async {
+          try {
+            final WriteBuffer allBytes = WriteBuffer();
+            for (Plane plane in cameraImage.planes) {
+              allBytes.putUint8List(plane.bytes);
+            }
+
+            final bytes = allBytes.done().buffer.asUint8List();
+            final Size imageSize = Size(
+                cameraImage.width.toDouble(), cameraImage.height.toDouble());
+
+            final InputImageRotation imageRotation =
+                InputImageRotation.Rotation_0deg;
+
+            final InputImageFormat inputImageFormat =
+                InputImageFormatMethods.fromRawValue(cameraImage.format.raw) ??
+                    InputImageFormat.NV21;
+
+            final planeData = cameraImage.planes.map(
+              (Plane plane) {
+                return InputImagePlaneMetadata(
+                  bytesPerRow: plane.bytesPerRow,
+                  height: plane.height,
+                  width: plane.width,
+                );
+              },
+            ).toList();
+
+            final inputImageData = InputImageData(
+              size: imageSize,
+              imageRotation: imageRotation,
+              inputImageFormat: inputImageFormat,
+              planeData: planeData,
+            );
+
+            final inputImageCamera = InputImage.fromBytes(
+                bytes: bytes, inputImageData: inputImageData);
+
+            await Future.delayed(Duration(seconds: 3));
+            await scannerBarCode(inputImageCamera);
+          } catch (e) {
+            print(e);
+          }
+        },
+      );
   }
 }
