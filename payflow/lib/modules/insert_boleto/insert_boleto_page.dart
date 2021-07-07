0a4c8df8ae91ab218dd15b51495a0d5a96353661
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-07 20:20:15 -0300

    add bottomNavigationBar cancelar / cadastrar

--- payflow/lib/modules/insert_boleto/insert_boleto_page.dart
+++ payflow/lib/modules/insert_boleto/insert_boleto_page.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -50,6 +51,12 @@ class InsertBoletoPage extends StatelessWidget {
             onChanged: (value) {},
           )
         ],
+      ),
+      bottomNavigationBar: SetLabelButtons(
+        primaryLabel: "Cancelar",
+        primaryOnPressed: () {},
+        secondaryLabel: "Cadastrar",
+        secondaryOnPressed: () {},
       ),
     );
   }
