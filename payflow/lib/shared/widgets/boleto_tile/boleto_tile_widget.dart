4ba197f075f06ec72a9800c1664c6c42909f3545
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 16:47:26 -0300

    add boleto tile widget

---
+++ payflow/lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class BoletoTileWidget extends StatelessWidget {
+  final BoletoModel boletoModel;
+  const BoletoTileWidget({Key? key, required this.boletoModel})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(boletoModel.name!),
+      subtitle: Text("Vence em ${boletoModel.dueDate}"),
+      trailing: Text.rich(
+        TextSpan(
+          text: "R\$ ",
+          style: TextStyles.trailingRegular,
+          children: [
+            TextSpan(
+              text: "${boletoModel.value!.toStringAsFixed(2)}",
+              style: TextStyles.trailingBold,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
