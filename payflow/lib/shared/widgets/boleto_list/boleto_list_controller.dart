705f1fb8fe7687fa1fa8c73b5ed57c690499c4c7
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 17:08:07 -0300

    add getBoletos

--- payflow/lib/shared/widgets/boleto_list/boleto_list_controller.dart
+++ payflow/lib/shared/widgets/boleto_list/boleto_list_controller.dart
@@ -1,5 +1,17 @@
 import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BoletoListController {
   List<BoletoModel> boletos = <BoletoModel>[];
+
+  Future<void> getBoletos() async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      final response = instance.getStringList("boletos") ?? <String>[];
+
+      boletos = response.map((e) => BoletoModel.fromJson(e)).toList();
+    } catch (e) {
+      boletos = <BoletoModel>[];
+    }
+  }
 }
