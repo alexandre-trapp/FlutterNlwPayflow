9977ebe498d1c1c718121efa8aa7abf17780d2fe
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 17:32:32 -0300

    add textRich in Row

--- payflow/lib/shared/widgets/boleto_info/boleto_info_widget.dart
+++ payflow/lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class BoletoInfoWidget extends StatelessWidget {
   final int size;
@@ -19,6 +20,27 @@ class BoletoInfoWidget extends StatelessWidget {
           color: AppColors.background,
           width: 56,
           height: 56,
+        ),
+        Container(
+          width: 1,
+          height: 32,
+          color: AppColors.background,
+        ),
+        Text.rich(
+          TextSpan(
+            text: "Você tem",
+            style: TextStyles.captionBackground,
+            children: [
+              TextSpan(
+                text: "$size boletos\n",
+                style: TextStyles.captionBoldBackground,
+              ),
+              TextSpan(
+                text: "cadastrados para pagar",
+                style: TextStyles.captionBackground,
+              ),
+            ],
+          ),
         ),
       ]),
     );
