dee83319f9b26c8f5ad46ab0c80267f8ccf5c2b6
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 17:19:30 -0300

    ajuste styles

--- payflow/lib/modules/meus_boletos/meus_boletos_page.dart
+++ payflow/lib/modules/meus_boletos/meus_boletos_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class MeusBoletosPage extends StatefulWidget {
   const MeusBoletosPage({Key? key}) : super(key: key);
@@ -11,7 +13,27 @@ class _MeusBoletosPageState extends State<MeusBoletosPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [Text("Meus boletos")],
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 24),
+          child: Row(
+            children: [
+              Text(
+                "Meus boletos",
+                style: TextStyles.titleBoldHeading,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          child: Divider(
+            thickness: 1,
+            height: 1,
+            color: AppColors.stroke,
+          ),
+        )
+      ],
     );
   }
 }
