9b50c0ea15e163092323e83f3270db78dd6b6de2
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 17:27:08 -0300

    add BoletoInfoWidget

---
+++ payflow/lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+class BoletoInfoWidget extends StatelessWidget {
+  final int size;
+  const BoletoInfoWidget({Key? key, required this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.secondary,
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+}
