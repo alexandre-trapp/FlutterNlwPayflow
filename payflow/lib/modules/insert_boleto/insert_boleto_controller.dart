61e36d09c66eeafa07ce165c9d6576ae3d116eab
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-09 16:11:16 -0300

    ajuste validação valor

--- payflow/lib/modules/insert_boleto/insert_boleto_controller.dart
+++ payflow/lib/modules/insert_boleto/insert_boleto_controller.dart
@@ -5,8 +5,8 @@ class InsertBoletoController {
   String? validateDueDate(String? value) =>
       value?.isEmpty ?? true ? "A data de vencimento não pode ser vazia" : null;
 
-  String? validateMoneyValue(String? value) =>
-      value?.isEmpty ?? true ? "Insira um valor maior que R\$ 0,00" : null;
+  String? validateMoneyValue(double value) =>
+      value == 0 ? "Insira um valor maior que R\$ 0,00" : null;
 
   String? validateBarcode(String? value) =>
       value?.isEmpty ?? true ? "O código do boleto não pode ser vazio" : null;
