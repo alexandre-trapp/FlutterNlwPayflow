bc29cad8459610d79f4bc47f91eff292f21de142
Alexandre Cordeiro Trapp <alexandre.trapp@ambevtech.com.br>
2021-07-12 22:13:11 -0300

    ajuste boas vindas

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
                     style: TextStyles.titleRegular,
                     children: [
                       TextSpan(
-                          text: "$widget.user.name",
+                          text: "${widget.user.name}",
                           style: TextStyles.titleBoldBackground)
                     ]),
               ),
